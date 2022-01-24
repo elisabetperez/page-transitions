962e9e04e6ff6397f5d2e8d256bc12bdd79f90b8
Eli <eli@landt.co>
2022-01-24 15:26:32 +0100

    mini repo

--- app/js/common.js
+++ app/js/common.js
@@ -1,18 +1,128 @@
 import './../scss/entries/common.scss';
-import Animation from './modules/animation/animation';
-import Blazy from 'blazy'; //inits the lazy loading for images
+import Highway from '@dogstudio/highway';
+import DefaultTransition from './modules/animation/transition.js';
+import IndexRenderer from './modules/animation/indexRenderer.js';
+import IntroRenderer from './modules/animation/introRenderer.js';
 
 class Common { 
-    constructor(){
-        new Animation();
-        new Blazy({ 
-			selector: ".b--lazy-a"
-		}); 
-        this.init();
+    constructor(){ 
+        const H = new Highway.Core({
+            renderers: {
+                index: IndexRenderer,
+                intro: IntroRenderer
+            },
+            transitions: {
+                default: DefaultTransition,
+            }
+        });
+        H.on('NAVIGATE_END', ({ to, from, trigger, location }) => {
+          this.manageStyles(to);
+          // this.manageScripts(to);
+        });
     }
 
-    init(){
-        console.log('common!!');
+    manageStyles(to) {
+        // Your main css file, used to prepend other styles
+        const main = document.querySelector('#introCommon');
+        const a = [...to.page.querySelectorAll('style:not([data-no-reload]), link:not([data-no-reload])')];
+        const b = [...document.querySelectorAll('style:not([data-no-reload]), link:not([data-no-reload])')];
+        // Compare Styles
+        for (let i = 0; i < b.length; i++) {
+          const c = b[i];
+          for (let j = 0; j < a.length; j++) {
+            const d = a[j];
+            if (c.outerHTML === d.outerHTML) {
+              // Create Shadow Style
+              const style = document.createElement(c.tagName);
+              // Loop Over Attributes
+              for (let k = 0; k < c.attributes.length; k++) {
+                // Get Attribute
+                const attr = c.attributes[k];
+                // Set Attribute
+                style.setAttribute(attr.nodeName, attr.nodeValue);
+              }
+              // Style Tag
+              if (c.tagName === 'STYLE') {
+                if (c.innerHTML) {
+                  style.innerHTML = c.innerHTML;
+                }
+              }
+              // Replace
+              c.parentNode.replaceChild(style, c);
+              // Clean Arrays
+              b.splice(i, 1);
+              a.splice(j, 1);
+              // Exit Loop
+              break;
+            }
+          }
+        }
+        // Remove Useless
+        for (const style of b) {
+          // Remove
+          style.parentNode.removeChild(style);
+        }
+        // Add Styles
+        for (const style of a) {
+          const loc = style.parentNode.tagName;
+          if (loc === 'HEAD') {
+            document.head.insertBefore(style, main);
+          }
+          if (loc === 'BODY') {
+            document.body.appendChild(style);
+          }
+        }
+    }
+
+    manageScripts(to) {
+      // Your main JS file, used to prepend other scripts
+      const main = document.querySelector('#main-script');
+      const a = [...to.page.querySelectorAll('script:not([data-no-reload])')];
+      const b = [...document.querySelectorAll('script:not([data-no-reload])')];
+      // Compare Scripts
+      for (let i = 0; i < b.length; i++) {
+        const c = b[i];
+        for (let j = 0; j < a.length; j++) {
+          const d = a[j];
+          if (c.outerHTML === d.outerHTML) {
+            // Create Shadow Script
+            const script = document.createElement(c.tagName);
+            // Loop Over Attributes
+            for (let k = 0; k < c.attributes.length; k++) {
+              // Get Attribute
+              const attr = c.attributes[k];
+              // Set Attribute
+              script.setAttribute(attr.nodeName, attr.nodeValue);
+            }
+            // Inline Script
+            if (c.innerHTML) {
+              script.innerHTML = c.innerHTML;
+            }
+            // Replace
+            c.parentNode.replaceChild(script, c);
+            // Clean Arrays
+            b.splice(i, 1);
+            a.splice(j, 1);
+            // Exit Loop
+            break;
+          }
+        }
+      }
+      // Remove Useless
+      for (const script of b) {
+        // Remove
+        script.parentNode.removeChild(script);
+      }
+      // Add Scripts
+      for (const script of a) {
+        const loc = script.parentNode.tagName;
+        if (loc === 'HEAD') {
+          document.head.appendChild(script);
+        }
+        if (loc === 'BODY') {
+          document.body.insertBefore(script, main);
+        }
+      }
     }
 
   
